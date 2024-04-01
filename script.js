fa73289d4a8900e59df65d7d44c78d3c9a501ddf
Ayushi Behl <ayushi.behl94@gmail.com>
2024-03-31 21:18:46 -0700

    Added comments in script.js file

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // JavaScript source code
 let formHandle = document.forms.add_teacher_form;
 
+//Call method on form submit event
 formHandle.onsubmit = sendRequestToServerToAddTeacher;
 
 function sendRequestToServerToAddTeacher(event){
     event.preventDefault();
 
+    //Collect form data
     let teacherData = {
         teacherfname: formHandle.firstName.value,
         teacherlname: formHandle.lastName.value,
@@ -16,17 +18,20 @@ function sendRequestToServerToAddTeacher(event){
 
     console.log(teacherData);
 
+    //Async call to POST API to add new teacher
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:50040/api/teachersData/addNewTeacher", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 4) {
-            console.log(xhr.reponseText);
+            
+            //On success, show success Response
             if(xhr.status === 200) {
                 console.log("success", xhr.responseText);
                 let successResponse = document.getElementById("successResponse");
                 successResponse.style.display = 'block';
-            } else {
+            } else { 
+                //On error, show error Response
                 console.log("Error from server", xhr.responseText);
                 let errorResponse = document.getElementById("errorResponse");
                 errorResponse.style.display = 'block';
